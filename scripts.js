281f4508038712c5a58fa45b75f449b71098ebb4
Rokas Stasiulis <rokastas@gmail.com>
2024-07-14 18:33:56 +0200

    added faq section

--- scripts.js
+++ scripts.js
@@ -25,3 +25,17 @@ document.addEventListener('DOMContentLoaded', function () {
     }
   });
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+  const faqItems = document.querySelectorAll('.faq-item');
+
+  faqItems.forEach(item => {
+      const question = item.querySelector('.faq-question');
+
+      question.addEventListener('click', () => {
+          const answer = item.querySelector('.faq-answer');
+
+          item.classList.toggle('active');
+      });
+  });
+});
