ce60eecfb7177e5130983df1d3a965fdeca49640
Rokas Stasiulis <rokastas@gmail.com>
2024-07-14 17:07:52 +0200

    added slider

--- scripts.js
+++ scripts.js
@@ -1,3 +1,27 @@
 function tryProductAlert() {
   alert("Mmm.. Delicious!");
 }
+
+function addToCartAlert() {
+  alert("Added to cart!");
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  const swiper = new Swiper('.swiper-container', {
+    direction: 'horizontal',
+    loop: true,
+
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      renderBullet: function (index, className) {
+        return '<span class="' + className + '"></span>';
+      },
+    },
+
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    }
+  });
+});
